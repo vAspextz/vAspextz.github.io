0f8b4e8bc0910c2d7ad0db23bac74b7813f2744b
vAspextz <codyosborne0909@gmail.com>
2024-12-16 11:20:01 -0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+      <h1>Welcome to My Vue App!</h1>
+      <p>This is a simple Vue app using Vite.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
